765ce640f41242cdbb74b406b96e42623a4b8d3f
ss-forkcoding <danju@starlight-sangdan.com>
2025-03-13 23:33:25 +0900

    feat: Add getFirstElement

--- src/array-utils.ts
+++ src/array-utils.ts
@@ -1,2 +1,7 @@
 export const isThereElements = (arr: any[]) => (arr.length === 0 ? true : false);
 export const isOverTheLength = (arr: any[], length: number) => (arr.length > length ? true : false);
+
+export const getFirstElement = <T>(arr: T[]) => {
+  if (isThereElements(arr)) return null;
+  return arr[0];
+};
